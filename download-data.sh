8ac8c9b9faef3e99d25d59d48ea39c872223db23
Kevin Ryan <kevinpa.ryan@gmail.com>
2026-07-29 11:08:58 +0100

    update download-data.sh to pull v2 and update md5sum

--- download-data.sh
+++ download-data.sh
@@ -2,7 +2,7 @@
 # check if the file already exists and has the correct md5 checksum
 if [ -f data/raw/hlamajority-appnote-data-raw.tar.gz ]; then
     echo "File already exists. Checking integrity..."
-    if echo "bff5f67e5f535e892dec74846f392e03  data/raw/hlamajority-appnote-data-raw.tar.gz" | md5sum -c -; then
+    if echo "22ab3109a1b0b4afb290395387169c39 data/raw/hlamajority-appnote-data-raw.tar.gz" | md5sum -c -; then
 	echo "File is valid. Skipping download - checking if it has been extracted..."
         if [ -d data/raw/cell-lines-before-polysolver-change ]; then 
             echo "Data has been extracted, skipping data extraction"
@@ -21,9 +21,9 @@ if [ -f data/raw/hlamajority-appnote-data-raw.tar.gz ]; then
     fi
 else
     echo "File does not exist. Downloading..."
-    wget -O data/raw/hlamajority-appnote-data-raw.tar.gz https://zenodo.org/records/19947258/files/hlamajority-appnote-data-raw.tar.gz?download=1
+    wget -O data/raw/hlamajority-appnote-data-raw.tar.gz https://zenodo.org/records/21653014/files/hlamajority-appnote-data-raw.tar.gz?download=1
     echo "Checking integrity of the downloaded file..."
-    if echo "bff5f67e5f535e892dec74846f392e03  data/raw/hlamajority-appnote-data-raw.tar.gz" | md5sum -c -; then
+    if echo "22ab3109a1b0b4afb290395387169c39 data/raw/hlamajority-appnote-data-raw.tar.gz" | md5sum -c -; then
         echo "File is valid."
         tar -xvf data/raw/hlamajority-appnote-data-raw.tar.gz -C data/raw/
         mv data/raw/raw/* data/raw/
